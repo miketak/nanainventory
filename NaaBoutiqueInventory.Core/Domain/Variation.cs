b3bfa8ed54b8989e57e0547128115d4b7fce21c0
miketak <mtakrama@yahoo.com>
2017-10-28 09:29:17 -0500

    Initial Commit - Front End and Project Structure

---
+++ NaaBoutiqueInventory.Core/Domain/Variation.cs
@@ -0,0 +1,9 @@
+﻿namespace NaaBoutiqueInventory.Core.Domain
+{
+    public class Variation
+    {
+        public int Id { get; set; }
+
+        public int Name { get; set; }
+    }
+}
---
+++ NaaBoutiqueInventory/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NaaBoutiqueInventory
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpload_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GenSku_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
